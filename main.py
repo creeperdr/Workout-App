bbfe7d023edf1349b9434417bd07c1b3ab7d13b9
creeperdr <drewday31@gmail.com>
2023-03-26 01:19:42 -0500

    3/25/2023

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import json
 
 from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton
-from kivymd.uix.card import MDCard
 from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import TwoLineRightIconListItem
 from kivymd.theming import ThemeManager
 
 from kivy.properties import StringProperty
@@ -47,7 +48,10 @@ class ColorSettingScreen(Screen):
 
 
 class WorkoutApp(MDApp):
+    starting = True
     theme_cls = ThemeManager()
+    add_workout_dialog = None
+    add_exercise_dialog = None
 
     def build(self):
         global settings
@@ -57,12 +61,28 @@ class WorkoutApp(MDApp):
         self.theme_cls.theme_style = settings["color_scheme"]["theme_style"]
         self.theme_cls.primary_palette = settings["color_scheme"]["primary_palette"]
         self.theme_cls.primary_hue = settings["color_scheme"]["primary_hue"]
+        self.after_start()
+
+    def after_start(self):
+        if self.starting:
+            for i in range(len(workouts)):
+                self.root.ids.workout_menu.ids['workout_container'].add_widget(WorkoutListItem(
+                    text='[b]' + str(list(workouts.keys())[i]) + '[/b]', secondary_text=str(len(dict(list(workouts.values())[i]).keys())) + ' exercises'))
+            self.starting = False
+        else:
+            pass
 
     def get_settings(self):
         with open("settings.json", 'r') as f:
             settings = json.load(f)
         f.close()
         return settings
+
+    def get_workouts(self):
+        with open('workouts.json', 'r') as f:
+            workouts = json.load(f)
+        f.close()
+        return workouts
 
     def change_color(self, primary):
         settings["color_scheme"]["primary_palette"] = primary
@@ -71,24 +91,50 @@ class WorkoutApp(MDApp):
         f.close()
         self.build()
 
-    def get_workouts(self):
-        with open('workouts.json', 'r') as f:
-            workouts = json.load(f)
-        f.close()
-        return workouts
+    def show_workout_dialog(self):
+        if not self.add_workout_dialog:
+            self.add_workout_dialog = MDDialog(
+                type="custom",
+                content_cls=WorkoutDialogContent(),
+            )
 
-    def create_new_workout(self):
+        self.add_workout_dialog.open()
+
+    def close_workout_dialog(self, *args):
+        self.add_workout_dialog.dismiss()
+
+    def create_new_workout(self, workout_name, number_exercises):
         new_workout = {}
-        for i in range(int(self.root.ids.add_workout.ids.number_exercises.text)):
+        for i in range(int(number_exercises)):
             new_exercise, info = self.add_exercise(i)
             new_workout[new_exercise] = info
-        self.add_workout(
-            self.root.ids.add_workout.ids.workout_name.text, new_workout)
+        self.add_workout(workout_name, new_workout)
+
+    def show_exercise_dialog(self):
+        if not self.add_exercise_dialog:
+            self.add_exercise_dialog = MDDialog(
+                type="custom",
+                content_cls=ExerciseDialogContent(),
+            )
+
+        self.add_exercise_dialog.open()
+
+    def add_exercise_field(self, checkbox, value):
+        if value:
+            print('timer')
+            # add_widget timer text field
+        else:
+            print('reps')
+            # add_widget reps text field
+
+    def close_exercise_dialog(self, *args):
+        self.add_exercise_dialog.dismiss()
 
     def add_exercise(self, x):
         name = str(x+1)
         stuff = {"timer": 0.0,
-                 "counterr": 0}
+                 "counter": 0}
+        self.show_exercise_dialog()
         # change to new popup, ask for the name of the exercise, ask if timer or counter,
         # get timer (float) or counter (int), submit button that adds to json file under the workout
         # *MAYBE USE MDDIALOG???*
@@ -96,6 +142,8 @@ class WorkoutApp(MDApp):
 
     def add_workout(self, name, exercises):
         workouts[name] = exercises
+        self.root.ids.workout_menu.ids['workout_container'].add_widget(WorkoutListItem(
+            text='[b]' + name + '[/b]', secondary_text=str(len(exercises.keys())) + ' exercises'))
         with open("workouts.json", "w") as f:
             json.dump(workouts, f, indent=4)
         f.close()
@@ -131,6 +179,29 @@ class WorkoutApp(MDApp):
         print(the_date.month)
         print(the_date.day)
     '''
+
+
+class WorkoutDialogContent(MDBoxLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class ExerciseDialogContent(MDBoxLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class WorkoutListItem(TwoLineRightIconListItem):
+
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        self.pk = pk
+
+    def delete_item(self, the_list_item):
+        self.parent.remove_widget(the_list_item)
+        # add delete from dictionary
 
 
 if __name__ == '__main__':
