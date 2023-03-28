70d34f74f02ac91d52b49984c584c03d84655222
creeperdr <drewday31@gmail.com>
2023-03-27 22:55:07 -0500

    3/27/2023

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 
 from kivymd.app import MDApp
@@ -6,6 +7,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.list import TwoLineRightIconListItem
+from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.textfield import MDTextField
 from kivymd.theming import ThemeManager
 
@@ -22,10 +24,6 @@ class CalendarMenuScreen(Screen):
 
 
 class WorkoutMenuScreen(Screen):
-    pass
-
-
-class AddWorkoutScreen(Screen):
     pass
 
 
@@ -54,12 +52,19 @@ class WorkoutApp(MDApp):
     theme_cls = ThemeManager()
     add_workout_dialog = None
     add_exercise_dialog = None
+    water_dialog = None
+    food_dialog = None
 
     def build(self):
         global settings
-        global workouts
         settings = self.get_settings()
+        global workouts
         workouts = self.get_workouts()
+        global water
+        water = self.get_water()
+        global foods
+        foods = self.get_foods()
+
         self.theme_cls.theme_style = settings["color_scheme"]["theme_style"]
         self.theme_cls.primary_palette = settings["color_scheme"]["primary_palette"]
         self.theme_cls.primary_hue = settings["color_scheme"]["primary_hue"]
@@ -71,6 +76,13 @@ class WorkoutApp(MDApp):
                 self.root.ids.workout_menu.ids['workout_container'].add_widget(WorkoutListItem(
                     text='[b]' + str(list(workouts.keys())[i]) + '[/b]', secondary_text=str(len(dict(list(workouts.values())[i]).keys())) + ' exercises'))
             self.starting = False
+            for i in range(len(water)):
+                self.root.ids.water_menu.ids['water_container'].add_widget(WaterListItem(
+                    text='[b]' + str(list(water.keys())[i]) + '[/b]', secondary_text=str(list(water.values())[i]) + ' gallons'))
+            self.starting = False
+            for i in range(len(foods)):
+                self.root.ids.food_menu.ids['food_container'].add_widget(FoodListItem(
+                    text='[b]' + str(list(foods.keys())[i]) + '[/b]', secondary_text=str(list(foods.values())[i]) + ' calories'))
         else:
             pass
 
@@ -85,6 +97,18 @@ class WorkoutApp(MDApp):
             workouts = json.load(f)
         f.close()
         return workouts
+
+    def get_water(self):
+        with open('water.json', 'r') as f:
+            water = json.load(f)
+        f.close()
+        return water
+
+    def get_foods(self):
+        with open('foods.json', 'r') as f:
+            foods = json.load(f)
+        f.close()
+        return foods
 
     def change_color(self, primary):
         settings["color_scheme"]["primary_palette"] = primary
@@ -121,14 +145,14 @@ class WorkoutApp(MDApp):
 
         self.add_exercise_dialog.open()
 
-    def add_exercise_field(self, checkbox, value):
-        if value:
-            print('timer')
-            self.add_exercise_dialog.add_widget(ExerciseListItem())
-            self.add_exercise_dialog.add_widget(ExerciseListItem())
-        else:
-            print('reps')
-            # add_widget reps text field
+#    def add_exercise_field(self, checkbox, value):
+#        if value:
+#            print('timer')
+#            self.add_exercise_dialog.add_widget(ExerciseListItem())
+#            self.add_exercise_dialog.add_widget(ExerciseListItem())
+#        else:
+#            print('reps')
+        # add_widget reps text field
 
     def close_exercise_dialog(self, *args):
         self.add_exercise_dialog.dismiss()
@@ -137,10 +161,9 @@ class WorkoutApp(MDApp):
         name = str(x+1)
         stuff = {"timer": 0.0,
                  "counter": 0}
-        self.show_exercise_dialog()
+        # self.show_exercise_dialog()
         # change to new popup, ask for the name of the exercise, ask if timer or counter,
         # get timer (float) or counter (int), submit button that adds to json file under the workout
-        # *MAYBE USE MDDIALOG???*
         return name, stuff
 
     def add_workout(self, name, exercises):
@@ -151,13 +174,47 @@ class WorkoutApp(MDApp):
             json.dump(workouts, f, indent=4)
         f.close()
 
-    def remove_workout(self, workout_list_item):
-        left = workout_list_item.split('[')
-        right = left[1].split(']')
-        x = workouts.pop(right[1])
-        with open("workouts.json", "w") as f:
-            json.dump(workouts, f, indent=4)
+    def show_water_dialog(self):
+        if not self.water_dialog:
+            self.water_dialog = MDDialog(
+                title="Add water to log",
+                type="custom",
+                content_cls=WaterDialogContent())
+
+        self.water_dialog.open()
+
+    def add_water(self, date, gallons_consumed):
+        water[str(date)] = int(gallons_consumed)
+        with open("water.json", "w") as f:
+            json.dump(water, f, indent=4)
         f.close()
+        self.root.ids.water_menu.ids['water_container'].add_widget(WaterListItem(
+            text='[b]' + date + '[/b]', secondary_text=gallons_consumed + ' gallon'))
+
+    def close_water_dialog(self):
+        self.water_dialog.dismiss()
+        self.water_dialog = None
+
+    def show_food_dialog(self):
+        if not self.food_dialog:
+            self.food_dialog = MDDialog(
+                title="Add calories to log",
+                type="custom",
+                content_cls=FoodDialogContent())
+
+        self.food_dialog.open()
+
+    def add_food(self, date, calories_consumed):
+        foods[str(date)] = int(calories_consumed)
+        with open("foods.json", "w") as f:
+            json.dump(foods, f, indent=4)
+        f.close()
+        self.root.ids.food_menu.ids['food_container'].add_widget(FoodListItem(
+            text='[b]' + date + '[/b]', secondary_text=calories_consumed + ' calories'))
+
+    def close_food_dialog(self):
+        self.food_dialog.dismiss()
+        self.food_dialog = None
 
     '''
     def show_alert_dialog(self):
@@ -204,16 +261,52 @@ class ExerciseDialogContent(MDBoxLayout):
         super().__init__(**kwargs)
 
 
+class WaterDialogContent(MDBoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # set the date_text label to today's date when useer first opens dialog box
+        self.ids.date_text.text = str(datetime.now().strftime('%A %d %B %Y'))
+
+    def show_date_picker(self):
+        """Opens the date picker"""
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save)
+        date_dialog.open()
+
+    def on_save(self, instance, value, date_range):
+        """This functions gets the date from the date picker and converts its it a
+        more friendly form then changes the date label on the dialog to that"""
+
+        date = value.strftime('%A %d %B %Y')
+        self.ids.date_text.text = str(date)
+
+
+class FoodDialogContent(MDBoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # set the date_text label to today's date when useer first opens dialog box
+        self.ids.date_text.text = str(datetime.now().strftime('%A %d %B %Y'))
+
+    def show_date_picker(self):
+        """Opens the date picker"""
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save=self.on_save)
+        date_dialog.open()
+
+    def on_save(self, instance, value, date_range):
+        """This functions gets the date from the date picker and converts its it a
+        more friendly form then changes the date label on the dialog to that"""
+
+        date = value.strftime('%A %d %B %Y')
+        self.ids.date_text.text = str(date)
+
+
 class ExerciseListItem(MDTextField):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def delete_item(self, exercise_list_item):
         self.parent.remove_widget(exercise_list_item)
-
-
-class Content(BoxLayout):
-    pass
 
 
 class WorkoutListItem(TwoLineRightIconListItem):
@@ -224,10 +317,16 @@ class WorkoutListItem(TwoLineRightIconListItem):
         self.pk = pk
 
     def delete_item(self, workout_list_item):
+        left = workout_list_item.text.split('[')
+        right = left[1].split(']')
+        x = workouts.pop(right[1])
+        with open("workouts.json", "w") as f:
+            json.dump(workouts, f, indent=4)
+        f.close()
         self.parent.remove_widget(workout_list_item)
+
+
         # add confirm dialog
-
-
 '''
     def confirm_delete(self, workout_list_item):
         if not self.confirm_delete_dialog:
@@ -257,6 +356,41 @@ class WorkoutListItem(TwoLineRightIconListItem):
         print("close")
         # self.confirm_delete_dialog.dismiss()
 '''
+
+
+class WaterListItem(TwoLineRightIconListItem):
+
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        self.pk = pk
+
+    def delete_item(self, water_list_item):
+        left = water_list_item.text.split('[')
+        right = left[1].split(']')
+        x = water.pop(right[1])
+        with open("water.json", "w") as f:
+            json.dump(water, f, indent=4)
+        f.close()
+        self.parent.remove_widget(water_list_item)
+        # add confirm dialog
+
+
+class FoodListItem(TwoLineRightIconListItem):
+
+    def __init__(self, pk=None, **kwargs):
+        super().__init__(**kwargs)
+        self.pk = pk
+
+    def delete_item(self, food_list_item):
+        left = food_list_item.text.split('[')
+        right = left[1].split(']')
+        x = foods.pop(right[1])
+        with open("foods.json", "w") as f:
+            json.dump(foods, f, indent=4)
+        f.close()
+        self.parent.remove_widget(food_list_item)
+        # add confirm dialog
+
 
 if __name__ == '__main__':
     WorkoutApp().run()
